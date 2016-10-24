69321c2e8eba38c22eafb82f8407c94d50f718e5
Joseph McMillin <jmcmillin40@gmail.com>
2016-10-24 16:58:27 -0500

    Can now parse movie data from JSON object

---
+++ src/Main.java
@@ -0,0 +1,153 @@
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+
+public class Main {
+	
+	static LinkedList<WantToSee> WantToSeeList = new LinkedList();
+	//CREATE have seen linkedlist HERE
+	//CREATE new release linkedlist HERE
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to your movie tracker");
+		MainMenu();
+		
+	}//end main
+	
+	
+	
+	
+	public static void MainMenu(){
+		while(true){
+			System.out.println();
+			System.out.println("MAIN MENU");
+			System.out.println("Option 1: view your movie lists");
+			System.out.println("Option 2: Add movies to your lists");
+			System.out.println("Option 3: Exit");
+			
+			Scanner scan = new Scanner(System.in);
+			String selection = scan.next();
+			
+			if(selection.equals("1")){
+				ListMenu();
+			}
+			if(selection.equals("2")){
+				AddMovies();
+			}
+			if(selection.equals("3")){
+				scan.close();
+				System.exit(0);
+			}
+			else{
+				System.out.println("Invalid input");
+			}
+		
+		
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void ListMenu(){
+		while(true){
+			System.out.println();
+			System.out.println("LIST MENU");
+			System.out.println("Option 1: view the movies you have seen");
+			System.out.println("Option 2: view the movies you want to see");
+			System.out.println("Option 3: view new releases");
+			System.out.println("Option 4: Go back to main menu");
+			
+			Scanner scan = new Scanner(System.in);
+			String selection = scan.next();
+			
+			if(selection.equals("1")){
+				//display movies in HAVE SEEN list
+				
+				MainMenu();
+			}
+			if(selection.equals("2")){
+				if(WantToSeeList.isEmpty()){
+					System.out.println("The list is empty");
+					MainMenu();
+				}
+				else{	
+					for(int i=0; i < WantToSeeList.size(); i++){
+					WantToSee movie = WantToSeeList.get(i);
+					System.out.println(movie.toString());
+					MainMenu();
+					}
+				}
+			}
+			if(selection.equals("3")){
+				//display movies in NEW RELEASES list
+				
+				MainMenu();
+			}
+			if(selection.equals("4")){ //Goes back to main menu
+				MainMenu();
+			}
+			else{ //returns control to while loop in this method 
+				System.out.println("Invalid input");
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void AddMovies(){
+		while(true){
+			String title;
+			String year;
+			System.out.println();
+			System.out.println("LIST MENU");
+			System.out.println("Option 1: Add to the moives you have seen");
+			System.out.println("Option 2: Add to the movies you want to see");
+			System.out.println("Option 3: Add moives to new releases");
+			System.out.println("Option 4: Go back to main menu");
+			
+			Scanner scan = new Scanner(System.in);
+			String selection = scan.next();
+			
+			Scanner sc = new Scanner(System.in).useDelimiter("\\n");  // USE THIS SCANNER OBJECT IN YOUR CODE BELOW
+			
+			if(selection.equals("1")){
+				//add movies to HAVE SEEN list
+				
+				MainMenu();
+			}
+			if(selection.equals("2")){
+				System.out.println("Enter a moive title");
+				title = sc.nextLine();
+				WantToSee movie = new WantToSee(title);
+				WantToSeeList.add(movie);
+				MainMenu();
+			}
+			if(selection.equals("3")){
+				//add movies to NEW RELEASES list
+				
+				MainMenu();
+			}
+			if(selection.equals("4")){ //Goes back to main menu
+				MainMenu();  
+			}
+			else{ //returns control to while loop in this method 
+				System.out.println("Invalid input");
+			}
+		}
+	}
+
+	
+	
+}
---
+++ src/WantToSee.java
@@ -0,0 +1,129 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+import org.json.*;
+
+public class WantToSee {
+	
+	private String MovieTitle;
+	private String ReleaseDate;
+	private String Plot;
+	private String IMDBRating;
+	
+	final String URL = "http://www.omdbapi.com/?";
+	final String CHARSET = "UTF-8";
+	
+	
+	public WantToSee(String title){
+		MovieTitle = title;
+		JSONObject output;
+		try {
+			output = SearchWithTitle(title);
+			ReleaseDate =  output.getString("Released");
+			Plot =  output.getString("Plot");
+			IMDBRating =  output.getString("imdbRating");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public WantToSee(String title, String year){
+		MovieTitle = title;
+		ReleaseDate = year;
+		String output;
+		try {
+			output = SearchWithTitleAndYear(title, year);
+			Plot = output;
+			IMDBRating = output;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	
+	public String getMovieTitle() {
+		return MovieTitle;
+	}
+
+	public void setMovieTitle(String movieTitle) {
+		MovieTitle = movieTitle;
+	}
+
+	public String getReleaseDate() {
+		return ReleaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		ReleaseDate = releaseDate;
+	}
+
+	public String getIMDBRating() {
+		return IMDBRating;
+	}
+
+	public void setIMDBRating(String rating) {
+		IMDBRating = rating;
+	}
+	
+	public String getPlot() {
+		return Plot;
+	}
+
+	public void setPlot(String plot) {
+		Plot = plot;
+	}
+
+	private JSONObject SearchWithTitle(String title) throws IOException, JSONException{
+		String query = String.format("t=%s",URLEncoder.encode(title, CHARSET));
+		    
+
+		URLConnection connection = new URL(URL + query).openConnection();
+		connection.setRequestProperty("Accept-Charset", CHARSET);
+		InputStream response = connection.getInputStream();
+		Scanner scanner = new Scanner(response); 
+		String responseBody = scanner.useDelimiter("\\A").next();
+		scanner.close();
+		JSONObject json = new JSONObject(responseBody);
+		//String name = obj.getString("Title");
+		//System.out.println(name);
+		return json;
+	}
+	
+	private String SearchWithTitleAndYear(String title, String year) throws IOException{
+		String query = String.format("t=%s&y=%s",URLEncoder.encode(title, CHARSET), year);
+		    
+
+		URLConnection connection = new URL(URL + query).openConnection();
+		connection.setRequestProperty("Accept-Charset", CHARSET);
+		InputStream response = connection.getInputStream();
+		Scanner scanner = new Scanner(response); 
+		String responseBody = scanner.useDelimiter("\\A").next();
+		scanner.close();
+		return responseBody;
+	}
+
+
+	@Override
+	public String toString() {
+		return "getMovieTitle()=" +  getMovieTitle()
+				+  "\n" +"getReleaseDate()=" + getReleaseDate() 
+				+ "\n" + "getPlot()=" + getPlot()
+				+ "\n" + "getIMDBRating()=" + getIMDBRating(); 
+				
+	}
+
+	
+
+}
